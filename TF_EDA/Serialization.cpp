2a947b101646dffc5b13f981869d3849d240f5c9
Piero010405 <84740254+Piero010405@users.noreply.github.com>
2024-06-23 01:32:15 -0500

    throw error

--- TF_EDA/Serialization.cpp
+++ TF_EDA/Serialization.cpp
@@ -3,7 +3,7 @@
 extern void SerializeBTree(const BTree& btree, const std::string& outputFilename) {
     std::ofstream outFile(outputFilename);
     if (!outFile.is_open()) {
-        std::cerr << "\t\tError al abrir el archivo de salida: " << outputFilename << " - " << strerror(errno) << std::endl;
+        throw std::runtime_error("\n\t\tNo se pudo abrir el archivo de ciudadanos para escribir");
         return;
     }
     boost::archive::text_oarchive oa(outFile);
@@ -14,8 +14,7 @@ extern void SerializeBTree(const BTree& btree, const std::string& outputFilename
 extern BTree DeserializeBTree(const std::string& inputFilename, int minimumDegree) {
     std::ifstream inFile(inputFilename);
     if (!inFile.is_open()) {
-        std::cerr << "\t\tError al abrir el archivo de entrada: " << inputFilename << " - " << strerror(errno) << std::endl;
-        throw std::runtime_error("No se pudo abrir el archivo de entrada.");
+        throw std::runtime_error("\n\t\tNo se pudo abrir el archivo de entrada.");
     }
     BTree btree(minimumDegree);
     boost::archive::text_iarchive ia(inFile);
